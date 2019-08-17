5ea0c987f06192eb4528ee4974553d6502b220ee
Jumas <kbbyfl91@gmail.com>
2019-08-17 22:11:53 +0300

    make app skeleton

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -15,46 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
-
-from django.urls import path
-from django.contrib import admin
-
-# Use include() to add URLS from the catalog application and authentication system
 from django.urls import include
+from django.views.generic import RedirectView
 
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
-
-urlpatterns += [
-    path('catalog/', include('catalog.urls')),
-]
-
-
-# Use static() to add url mapping to serve static files during development (only)
 from django.conf import settings
 from django.conf.urls.static import static
 
-
-urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
-#Add URL maps to redirect the base URL to our application
-from django.views.generic import RedirectView
-urlpatterns += [
+urlpatterns = [
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
-]
-
-
-
-#Add Django site authentication urls (for login, logout, password management)
-urlpatterns += [
-    path('accounts/', include('django.contrib.auth.urls')),
-]
+    path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- locallibrary/locallibrary/wsgi.py
+++ locallibrary/locallibrary/wsgi.py
@@ -1,4 +1,5 @@
-"""WSGI config for locallibrary project.
+"""
+WSGI config for locallibrary project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
