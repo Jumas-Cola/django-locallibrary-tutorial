b7e7f4369e20169a415c3179363fcf1801bdf4ec
Jumas-Cola <kbbyfl91@gmail.com>
2019-08-22 06:31:06 +0300

    add 5 lesson

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 
 def index(request):
@@ -14,6 +15,10 @@ def index(request):
         status__exact='a').count()
     # Метод 'all()' применен по умолчанию.
     num_authors = Author.objects.count()
+    journey_books = Book.objects.filter(
+        title__icontains='утешест').count()
+    poetry_genres = Genre.objects.filter(
+        name__icontains='оэзи').count()
 
     # Отрисовка HTML-шаблона index.html с данными внутри
     # переменной контекста context
@@ -24,6 +29,26 @@ def index(request):
             'num_books': num_books,
             'num_instances': num_instances,
             'num_instances_available': num_instances_available,
-            'num_authors': num_authors
+            'num_authors': num_authors,
+            'journey_books': journey_books,
+            'poetry_genres': poetry_genres
         },
     )
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 3
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 3
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
